0277ee037a3abb38f4196d8da5373140196b337c
Isaac Halvorson <hello@hisaac.net>
2024-09-01 15:18:22 -0500

    Add some bits and bobs for GitHub

--- Package.swift
+++ Package.swift
@@ -9,24 +9,22 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.5.0"),
-		.package(url: "https://github.com/cpisciotta/xcbeautify.git", exact: "2.11.0"),
-		.package(url: "https://github.com/tuist/Command.git", exact: "0.7.4"),
-		.package(url: "https://github.com/tuist/FileSystem.git", exact: "0.2.0"),
 		.package(url: "https://github.com/tuist/Path.git", exact: "0.3.2"),
-		.package(url: "https://github.com/tuist/XcodeProj.git", exact: "8.23.0"),
-		// https://github.com/tuist/SwiftTerminal
 	],
 	targets: [
 		.executableTarget(
 			name: "xcb",
 			dependencies: [
 				.product(name: "ArgumentParser", package: "swift-argument-parser"),
-				.product(name: "Command", package: "Command"),
-				.product(name: "FileSystem", package: "FileSystem"),
 				.product(name: "Path", package: "Path"),
-				.product(name: "xcbeautify", package: "xcbeautify"),
-				.product(name: "XcodeProj", package: "XcodeProj"),
 			]
 		),
 	]
 )
+
+// Packages for future reference:
+// - https://github.com/cpisciotta/xcbeautify
+// - https://github.com/tuist/Command
+// - https://github.com/tuist/FileSystem
+// - https://github.com/tuist/XcodeProj
+// - https://github.com/tuist/SwiftTerminal
---
+++ README.md
@@ -0,0 +1,21 @@
+# `xcb`
+
+`xcb` is a Swift CLI for interacting with the `xcodebuild` and `swift` CLI tools. It aims to provide nicer defaults, new features, and improved ergonomics.
+
+> [!WARNING]
+> This project is in the very early stages of development, and is not yet ready for real world usage.
+
+## Goals
+
+### Nicer Defaults
+
+### New Features
+
+- Ensure usage of the desired Xcode / Swift version if specified.
+
+### Improved Ergonomics
+
+- Define build settings in a config file.
+- Easier selection of schemes and devices.
+- Improved output formatting using [`xcbeautify`](https://github.com/cpisciotta/xcbeautify/).
+- Common errors will automatically detected and displayed in a user-friendly manner.
