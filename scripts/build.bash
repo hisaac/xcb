6e9a10ac266fbbee10451bf38cf283f947ddfaf2
Isaac Halvorson <hello@hisaac.net>
2024-09-01 22:11:08 -0500

    Revert "Use xcbeautify"
    This reverts commit a323bf60d351a608093a3f23a82fb082e0df03aa.

--- scripts/build.bash
+++ scripts/build.bash
@@ -4,10 +4,7 @@ source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/lib/base.bash"
 trap 'exit_handler "$?" "${0##*/}"' EXIT
 
 function main() {
-	xcrun_exec swift build \
-		--package-path "${PROJECT_ROOT}" \
-		"$@" |
-		mise_exec xcbeautify
+	xcrun_exec swift build --package-path "${PROJECT_ROOT}" "$@"
 }
 
 main "$@"
